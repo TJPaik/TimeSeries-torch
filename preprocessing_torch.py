9129551cf7fb9d45b61bfc3a79dbe6b75af140fc
paiktj <paiktj@gmail.com>
2021-12-13 13:52:00 +0900

    Feat: Padding options for GaussianFilter1D and MedianFilter1D and Hz option for EcgPreprocess
    OtherThings: Example changed

--- preprocessing_torch.py
+++ preprocessing_torch.py
@@ -4,47 +4,48 @@ from torch import nn
 
 
 class GaussianFilter1D(nn.Module):
-    def __init__(self, sigma, fixed_length: int = -1):
+    def __init__(self, sigma, length, truncate=4, mode='replicate'):
+        """
+        :param sigma:
+        :param length:
+        :param truncate:
+        :param mode: 'zeros', 'reflect', 'replicate' or 'circular'. Default: 'zeros'
+        """
         super(GaussianFilter1D, self).__init__()
         self.P = Normal(torch.tensor([0]), torch.tensor([sigma]))
-        if fixed_length != -1:
-            assert fixed_length > 0
-            weight = self.P.log_prob(torch.arange(-fixed_length, fixed_length)).exp()
-            self.conv = nn.Conv1d(1, 1, kernel_size=2 * fixed_length, bias=False)
-            self.conv.weight = nn.Parameter(
-                weight[None, None, :]
-            )
-            self.conv.requires_grad_(False)
-        else:
-            raise NotImplementedError()
+        assert length > 0
+        kernel_length = int(truncate * sigma + 0.5)
+        weight = self.P.log_prob(torch.arange(-kernel_length, kernel_length + 1)).exp()
+        self.conv = nn.Conv1d(1, 1, kernel_size=len(weight), bias=False, padding=kernel_length, padding_mode=mode)
+        self.conv.weight = nn.Parameter(
+            weight[None, None, :] / torch.sum(weight)
+        )
+        self.conv.requires_grad_(False)
 
     def forward(self, x):
-        x = torch.cat(
-            (
-                torch.flip(x, (2,)),
-                x,
-                torch.flip(x[:, :, 1:], (2,))
-            ),
-            dim=2
-        )
-        x_shape = x.shape
         x = self.conv(
             x.view(
-                x_shape[0] * x_shape[1], 1, -1
+                x.shape[0] * x.shape[1], 1, -1
             )
-        ).view(*x_shape[:2], -1)
+        ).view(*x.shape[:2], -1)
         return x
 
 
 class MedianFilter1D(nn.Module):
-    def __init__(self, kernel_size: int):
+    def __init__(self, kernel_size: int, padding_mode='replicate'):
         super(MedianFilter1D, self).__init__()
+
         assert kernel_size % 2 and kernel_size > 0
         self.kernel_size = kernel_size
         self.Unfold = nn.Unfold(kernel_size=(1, kernel_size))
-        # self.Padding = nn.ReflectionPad1d(padding=(kernel_size - 1) // 2)
-        # self.Padding = nn.ReplicationPad1d(padding=(kernel_size - 1) // 2)
-        self.Padding = nn.ConstantPad1d(padding=(kernel_size - 1) // 2, value=0.0)
+        if padding_mode == 'replicate':
+            self.Padding = nn.ReplicationPad1d(padding=(kernel_size - 1) // 2)
+        elif padding_mode == 'reflect':
+            self.Padding = nn.ReflectionPad1d(padding=(kernel_size - 1) // 2)
+        elif padding_mode == 'zeros':
+            self.Padding = nn.ConstantPad1d(padding=(kernel_size - 1) // 2, value=0.0)
+        else:
+            raise NotImplementedError()
 
     def forward(self, x):
         unfolded = self.Unfold(
@@ -58,16 +59,19 @@ class MedianFilter1D(nn.Module):
 
 class EcgPreprocess(nn.Module):
     '''
+    500 hz ECG
     reference :
     Advances in Cardiac Signal Processing
     Book by Rajendra Acharya U
     '''
 
-    def __init__(self):
+    def __init__(self, Hz=500):
         super(EcgPreprocess, self).__init__()
 
-        self.MF1 = MedianFilter1D(101)
-        self.MF2 = MedianFilter1D(301)
+        self.param1 = round((Hz / 5 - 1) / 2) * 2 + 1
+        self.param2 = round(((Hz * 3 / 5) - 1) / 2) * 2 + 1
+        self.MF1 = MedianFilter1D(self.param1)
+        self.MF2 = MedianFilter1D(self.param2)
 
     def forward(self, x):
         y = self.MF2(self.MF1(x))
@@ -78,37 +82,9 @@ if __name__ == '__main__':
     import numpy as np
     import matplotlib.pyplot as plt
 
-    from scipy.signal import medfilt
-    from scipy.ndimage import gaussian_filter1d
     from scipy.misc import electrocardiogram
 
-    dummy_input = torch.rand(3, 6, 21)
-    SIGMA = 2
-    GF = GaussianFilter1D(sigma=SIGMA, fixed_length=21)
-    output = GF(dummy_input).detach()
-    original_output = gaussian_filter1d(dummy_input, sigma=SIGMA, axis=2)
-    print('Gaussian Filter maximal error:', torch.max(torch.abs(torch.as_tensor(original_output) - output)).item())
-
-    plt.plot(output[1, 4].detach(), label='torch version')
-    plt.plot(original_output[1, 4], label='original')
-    plt.title('Gaussian filter test')
-    plt.legend()
-    plt.show()
-
-    dummy_input = torch.rand(3, 6, 28)
-    kernel_size = 5
-    MF = MedianFilter1D(kernel_size)
-    output = MF(dummy_input).detach()
-    original_output = np.asarray([[medfilt(dummy_input[i, j], kernel_size) for j in range(6)] for i in range(3)])
-    print('Median Filter maximal error:', torch.max(torch.abs(torch.as_tensor(original_output) - output)).item())
-
-    plt.plot(output[1, 4].detach(), label='torch version')
-    plt.plot(original_output[1, 4], label='original')
-    plt.title('Median Filter test')
-    plt.legend()
-    plt.show()
-
-    x = np.asarray([[
+    ECG_input = np.asarray([[
         electrocardiogram()[2000:4000],
         electrocardiogram()[6000:8000],
         electrocardiogram()[8000:10000]
@@ -116,11 +92,21 @@ if __name__ == '__main__':
         electrocardiogram()[12000:14000],
         electrocardiogram()[16000:18000],
         electrocardiogram()[18000:20000]
-    ]])
-    EP = EcgPreprocess()
-    x_preprocessed = EP(torch.as_tensor(x))
+    ]])  # 360 Hz
+    print('ECG input shape :', ECG_input.shape)
 
-    plt.plot(x[0, 0], label='original')
-    plt.plot(x_preprocessed[0, 0], label='debased')
-    plt.legend()
+    EP = EcgPreprocess(360)
+    EP_output = EP(torch.as_tensor(ECG_input)).detach()
+
+    GF = GaussianFilter1D(2, 2000)
+    GF_output = GF(EP_output.float())
+
+    fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+    axs[0].plot(ECG_input[0, 0])
+    axs[0].set_title('original')
+    axs[1].plot(EP_output[0, 0])
+    axs[1].set_title('baseline wander remover')
+    axs[2].plot(GF_output[0, 0])
+    axs[2].set_title('denoise')
+    plt.tight_layout()
     plt.show()
